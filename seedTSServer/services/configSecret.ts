dbf0026695cfb315814f9b16e33e01491616bd6c
Richard Lasjunies <rlasjunies@gmail.com>
2015-01-18 16:40:02 +0100

    Revert "na"
    This reverts commit 76aa913fc8a94551c04920dafd48fce7c56f0151.

--- seedTSServer/services/configSecret.ts
+++ seedTSServer/services/configSecret.ts
@@ -1,9 +1,9 @@
 ﻿var secret = {
-    FACEBOOK_SECRET: "bd245183dab16245b41925c469ea593e",
-    GOOGLE_SECRET: "IOL253SoCTPbmivOXlREdlgV",
+    FACEBOOK_SECRET: "x",
+    GOOGLE_SECRET: "y",
     JWT_SECRET: "SECRET",
     EMAIL_SECRET: "SECRET",
-    SMTP_PASS: "L@sjunies01",
+    SMTP_PASS: "a",
     db: {
         production: "rlasjunies:1234",
         development: "",
