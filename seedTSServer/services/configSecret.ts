76aa913fc8a94551c04920dafd48fce7c56f0151
Richard Lasjunies <rlasjunies@gmail.com>
2015-01-18 16:39:08 +0100

    na

--- seedTSServer/services/configSecret.ts
+++ seedTSServer/services/configSecret.ts
@@ -1,9 +1,9 @@
 ﻿var secret = {
-    FACEBOOK_SECRET: "x",
-    GOOGLE_SECRET: "y",
+    FACEBOOK_SECRET: "bd245183dab16245b41925c469ea593e",
+    GOOGLE_SECRET: "IOL253SoCTPbmivOXlREdlgV",
     JWT_SECRET: "SECRET",
     EMAIL_SECRET: "SECRET",
-    SMTP_PASS: "a",
+    SMTP_PASS: "L@sjunies01",
     db: {
         production: "rlasjunies:1234",
         development: "",
